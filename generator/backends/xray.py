17fa9608bc345790ca94d8f0d0a2ec258290ef08
Ivan <70050258+KoZaVka229@users.noreply.github.com>
2026-06-26 20:53:02 +0500

    fix error

--- generator/backends/xray.py
+++ generator/backends/xray.py
@@ -8,7 +8,7 @@ from ..users import get_or_create_uid
 def setup_xray(credentials, xray_privkey):
     Path('data').mkdir(parents=True, exist_ok=True)
 
-    clients = [{"id": get_or_create_uid(username), "flow": "xtls-rprx-vision"} for username in credentials.keys]
+    clients = [{"id": get_or_create_uid(username), "flow": "xtls-rprx-vision"} for username in credentials.keys()]
 
     with open("templates/xray.json") as f:
         config = json.load(f)
